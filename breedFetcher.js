a72fc60c40bac90f2081fae57fac25827edf29b1
Brian <bshin132@gmail.com>
2021-12-03 05:08:15 +0000

    refactoring done

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,38 @@
 const request = require("request");
 const catBreed = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`,
-  (error, response, body) => {
-    if (error) {
-      console.log("ERROR");
-      return error;
-    }
-    const data = JSON.parse(body);
-    if (data.length <= 0) {
-      console.log("Breed not found");
-      return;
-    }
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      //WITH INSTRUCTOR
+      if (error != null) {
+        callback(error, null)
+      } else {
+        data = JSON.parse(body);
+        if (data.length === 0) {
+          callback("Breed not found", null)
+        } else {
+          callback(null, data[0].description)
+        }
+      }
 
-    const description = data[0].description;
-    console.log(description);
-    console.log(typeof data);
-  }
-);
+
+
+      //MY CODE
+      // console.log(error)
+      // callback(error, null)
+      // let data = null;
+      // if (!error) {
+      //   if (JSON.parse(body).length === 0) {
+      //     error = "Breed not found";
+      //   } else {
+      //     data = JSON.parse(body)[0]["description"];
+      //   }
+      // }
+      // callback(error, data);
+    }
+  );
+};
+
+module.exports = { fetchBreedDescription };
